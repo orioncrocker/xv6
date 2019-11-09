2871835ff655fd92f22d26a2d4a1f0df76f26233
orion <orioncrocker@gmail.com>
2019-11-09 14:39:41 -0800

    added one test, need to add a few others

--- p3-test.c
+++ p3-test.c
@@ -1,26 +1,26 @@
 #ifdef CS333_P3
+#include "types.h"
 #include "user.h"
 
 void
-roundRobinTest(){
+zombieTest(){
 
-  int proc;
-  char next;
+  int n;
+  char *next = 0;
 
-  proc = fork();
-  if (proc == 0){
-    while((proc = fork()) == 0);
-    if(proc < 0){
-      printf("Maximum processes reached. Press key to continue.\n");
-      scan("%c", next);
-      wait();
+  for(n = 0; n < 10; n++){
+    if(fork() == 0)
       exit();
-    }
   }
+  printf(1, "%d zombies should exist. Check ctl-Z\n", n);
+  gets(next, 10);
+  for(n = 0; n < 10; n++)
+    wait();
 }
 
 int
 main(int argc, char *argv[]){
-  roundRobinTest();
+  zombieTest();
+  exit();
 }
 #endif
