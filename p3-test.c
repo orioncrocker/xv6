b82d31c1c02df2fb87d3fdd1f97f758d7eb6f8b3
orion <orioncrocker@gmail.com>
2019-11-09 17:49:34 -0800

    updated zombieTest to take int argument, added sleepTest

--- p3-test.c
+++ p3-test.c
@@ -3,24 +3,70 @@
 #include "user.h"
 
 void
-zombieTest(){
+zombieTest(int n){
 
-  int n;
+  int i;
+  int extra = 0;
   char *next = 0;
 
-  for(n = 0; n < 10; n++){
-    if(fork() == 0)
+  for(i = 0; i < n; i++){
+    int ret = fork();
+    if(ret == -1)
+      extra++;
+    if(ret == 0)
       exit();
   }
-  printf(1, "%d zombies should exist. Check ctl-Z\n", n);
+
+  if(extra){
+    printf(1, "Created %d too many child procs, not enough space.\n", extra);
+    n = n-extra;
+  }
+
+  printf(1, "%d zombies should exist. "
+         "Check lists or press any key to reap child procs.\n", n);
   gets(next, 10);
-  for(n = 0; n < 10; n++)
+
+  int reaped = 0;
+  for(i = 0; i < n; i++){
     wait();
+    reaped++;
+  }
+  printf(1, "%d procs reaped.\n", reaped);
+}
+
+void
+sleepTest(void){
+
+  int ret = fork();
+  char *next = 0;
+
+  if(ret == 0) {
+    int count = 0;
+    while((ret = fork()) == 0)
+      count++;
+    if(ret > 0){
+      wait();
+      exit();
+    }
+    printf(1, "%d sleeping procs should exist. "
+           "Check lists or press any key to reap child procs.\n", count);
+    gets(next, 10);
+    exit();
+  }
+  wait();
 }
 
 int
 main(int argc, char *argv[]){
-  zombieTest();
+
+  if(argc == 1){
+    zombieTest(10);
+    //runningTest(10);
+  } else {
+    int n = atoi(argv[1]);
+    zombieTest(n);
+  }
+  sleepTest();
   exit();
 }
 #endif
