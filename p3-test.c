fe3a252b535701761a96b76b06d9d49f2cef083c
orion <orioncrocker@gmail.com>
2019-11-09 19:03:21 -0800

    added runTest(). all tests are satisfactory

--- p3-test.c
+++ p3-test.c
@@ -5,12 +5,12 @@
 void
 zombieTest(int n){
 
-  int i;
+  int i, ret;
   int extra = 0;
   char *next = 0;
 
   for(i = 0; i < n; i++){
-    int ret = fork();
+    ret = fork();
     if(ret == -1)
       extra++;
     if(ret == 0)
@@ -37,10 +37,10 @@ zombieTest(int n){
 void
 sleepTest(void){
 
-  int ret = fork();
   char *next = 0;
+  int ret = fork();
 
-  if(ret == 0) {
+  if(ret == 0){
     int count = 0;
     while((ret = fork()) == 0)
       count++;
@@ -56,17 +56,72 @@ sleepTest(void){
   wait();
 }
 
+void
+runTest(int n){
+
+  int i, ret;
+  int extra = 0;
+  char *next = 0;
+
+  printf(1, "Press any key to begin run test. Use ctl-r over the next few seconds.\n");
+  gets(next, 10);
+
+  for(i = 0; i < n; i++){
+    ret = fork();
+    if(ret == -1)
+      extra++;
+    if(ret == 0) {
+      for(int i = 0; i < 100000000; i++);
+      exit();
+    }
+  }
+
+
+/*
+  int ret = fork();
+  if (ret == 0){
+    while((ret = fork()) == 0);
+    for(int i = 0; i < 10000000; i++);
+
+    if(ret > 0){
+      wait();
+      exit();
+    }
+*/
+  if(extra)
+    n = n-extra;
+
+  for(i = 0; i < n; i++)
+    wait();
+
+  printf(1, "Test complete. Press any key to continue.\n");
+  gets(next, 10);
+}
+
 int
 main(int argc, char *argv[]){
 
-  if(argc == 1){
-    zombieTest(10);
-    //runningTest(10);
-  } else {
-    int n = atoi(argv[1]);
+  // default number of procs to test
+  int n = 10;
+
+  if(argc <= 1){
     zombieTest(n);
+    runTest(n);
+    sleepTest();
+
+  } else {
+
+    for (uint i = 1; i < argc; i++) {
+      if (strcmp(argv[i], "zombie") == 0 || strcmp(argv[i], "z") == 0)
+        zombieTest(n);
+      else if (strcmp(argv[i], "run") == 0 || strcmp(argv[i], "r") == 0)
+        runTest(n);
+      else if (strcmp(argv[i], "sleep") == 0 || strcmp(argv[i], "s") == 0)
+        sleepTest();
+      else
+        printf(1, "Command not recognized, check p3-test.c for examples\n");
+    }
   }
-  sleepTest();
   exit();
 }
 #endif
